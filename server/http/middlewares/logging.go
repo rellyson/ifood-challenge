a350a3966355615e62bebb88c2cf4a5a38b39684
Rellyson Silva <rellyson@protonmail.com>
2022-04-02 14:54:04 -0300

    logging middleware improved

--- server/http/middlewares/logging.go
+++ server/http/middlewares/logging.go
@@ -3,6 +3,7 @@ package middlewares
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
 )
@@ -13,18 +14,26 @@ type LoggingPayload struct {
 	RequestURL string              `json:"request_url"`
 	UserAgent  string              `json:"user_agent"`
 	Headers    map[string][]string `json:"headers"`
+	Body       string              `json:"body"`
 }
 
 func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		p := LoggingPayload{}
 		p.Date = time.Now()
-		p.RequestURL = r.RequestURI
-		p.Headers = r.Header
 		p.Method = r.Method
+		p.RequestURL = r.RequestURI
 		p.UserAgent = r.UserAgent()
+		p.Headers = r.Header
+		p.Body = convertBodyToString(r.Body)
 
 		v, _ := json.Marshal(&p)
 		fmt.Println(string(v))
 	})
+}
+
+func convertBodyToString(b io.ReadCloser) string {
+	body, _ := io.ReadAll(b)
+
+	return fmt.Sprintf("%v", string(body))
 }
