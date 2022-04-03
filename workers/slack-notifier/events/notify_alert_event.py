ec3c06bff20200cc0bd63eb785e19f1daa4bf5cf
Rellyson Silva <rellyson@protonmail.com>
2022-04-03 16:12:38 -0300

    event loop fixed

--- workers/slack-notifier/events/notify_alert_event.py
+++ workers/slack-notifier/events/notify_alert_event.py
@@ -8,19 +8,20 @@ class NotifyAlertEvent:
 
         print("Started listenning to messages at: {}".format(queue_url))
 
-        messages = self.sqs_client.receive_message(
-            QueueUrl=queue_url,
-            VisibilityTimeout=0,
-            WaitTimeSeconds=0
-        )
-
-        for message in messages.get("Messages", []):
-            receipt_handle = message['ReceiptHandle']
-            message_body = json.loads(message["Body"])
-
-            print("Received Message: {}".format(message_body))
-
-            self.sqs_client.delete_message(
+        while True:
+            messages = self.sqs_client.receive_message(
                 QueueUrl=queue_url,
-                ReceiptHandle=receipt_handle
+                VisibilityTimeout=0,
+                WaitTimeSeconds=0
             )
+
+            for message in messages.get("Messages", []):
+                receipt_handle = message['ReceiptHandle']
+                message_body = json.loads(message["Body"])
+
+                print("Received Message: {}".format(message_body))
+
+                self.sqs_client.delete_message(
+                    QueueUrl=queue_url,
+                    ReceiptHandle=receipt_handle
+                )
