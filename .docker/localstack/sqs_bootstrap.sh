a7f5d0a505cd8f0089a2b35ad6b6283bcdf5e2af
Rellyson Silva <rellyson@protonmail.com>
2022-04-04 00:29:13 -0300

    notification dlq configured

--- .docker/localstack/sqs_bootstrap.sh
+++ .docker/localstack/sqs_bootstrap.sh
@@ -6,4 +6,16 @@ export AWS_ACCESS_KEY_ID=foobar
 export AWS_SECRET_ACCESS_KEY=foobar
 
 # Create queues
-aws --endpoint-url=$LOCALSTACK_ENDPOINT sqs create-queue --queue-name notify_alert_event --region $AWS_DEFAULT_REGION
+
+# Dead letter queue
+aws --endpoint-url=$LOCALSTACK_ENDPOINT sqs create-queue --queue-name notification_dlx --region $AWS_DEFAULT_REGION # DLX queue
+DLQ_SQS_ARN=$(aws --endpoint-url=$LOCALSTACK_ENDPOINT sqs get-queue-attributes\
+                  --attribute-name QueueArn --queue-url=$LOCALSTACK_ENDPOINT/000000000000/notification_dlx\
+                  |  sed 's/"QueueArn"/\n"QueueArn"/g' | grep '"QueueArn"' | awk -F '"QueueArn":' '{print $2}' | tr -d '"' | xargs)
+
+
+# event queue 
+aws --endpoint-url=$LOCALSTACK_ENDPOINT sqs create-queue --queue-name notify_alert_event --region $AWS_DEFAULT_REGION \
+    --attributes '{
+                    "RedrivePolicy": "{\"deadLetterTargetArn\":\"'"$DLQ_SQS_ARN"'\",\"maxReceiveCount\":\"10\"}",
+                    }'
--- workers/slack-notifier/aws/sqs_client.py
+++ workers/slack-notifier/aws/sqs_client.py
@@ -3,17 +3,15 @@ import boto3
 import os
 
 
-def SQSClient():
+def SQSClient() -> boto3.client:
     region = os.environ.get('AWS_REGION')
     endpoint = os.environ.get('AWS_ENDPOINT')
     access_id = os.environ.get('AWS_ACCESS_KEY_ID')
     access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-    if(endpoint == None or region == None):
-        fatal("Missing required env variables")
-    sqs = boto3.client("sqs", region_name=region,
-                       endpoint_url=endpoint,
-                       aws_access_key_id=access_id,
-                       aws_secret_access_key=access_key)
-
-    return sqs
+    if(endpoint == None or region == None or access_id == None or access_key == None):
+        fatal("Missing AWS required environment variables")
+    return boto3.client("sqs", region_name=region,
+                        endpoint_url=endpoint,
+                        aws_access_key_id=access_id,
+                        aws_secret_access_key=access_key)
