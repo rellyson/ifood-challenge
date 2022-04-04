2ce3c4b981b3b7037371afe597f9540763cbfaeb
Rellyson Silva <rellyson@protonmail.com>
2022-04-03 23:22:08 -0300

    slack service test added

--- workers/slack-notifier/services/slack_service.py
+++ workers/slack-notifier/services/slack_service.py
@@ -1,12 +1,12 @@
-import os
-from dataclasses import dataclass
+from typing import Any
 from abc import ABCMeta, abstractmethod
+from dataclasses import dataclass
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 
 @dataclass
-class MessagePayload:
+class PostMessagePayload:
     channel: str
     text: str
 
@@ -15,19 +15,19 @@ class SlackServiceInterface:
     __metaclass__ = ABCMeta
 
     @abstractmethod
-    async def post_message(self, message: MessagePayload) -> None: pass
+    async def post_message(self, message: PostMessagePayload) -> Any: pass
 
 
 class SlackService(SlackServiceInterface):
-    def __init__(self) -> None:
-        self.web_client = WebClient(token=os.environ.get('SLACK_BOT_TOKEN'))
+    def __init__(self, client: WebClient):
+        self.web_client = client
 
-    async def post_message(self, message: MessagePayload):
+    async def post_message(self, message: PostMessagePayload):
         try:
             response = self.web_client.chat_postMessage(
                 channel=message.channel, text=message.text)
-            assert response["message"]["text"] == message.text
 
+            return response["message"]
         except SlackApiError as e:
             print("There was an error: {}".format(e.response['error']))
             raise
---
+++ workers/slack-notifier/tests/test_slack_service.py
@@ -0,0 +1,38 @@
+import asyncio
+from more_itertools import side_effect
+import pytest
+from unittest.mock import patch
+from slack_sdk.errors import SlackApiError
+from slack_sdk import WebClient
+from services.slack_service import SlackService, PostMessagePayload
+
+
+@pytest.fixture
+def mock_service():
+    return SlackService(WebClient())
+
+
+@patch('services.slack_service.WebClient.chat_postMessage')
+def test_post_message_succesful_response(mock_client, mock_service):
+
+    payload = PostMessagePayload(channel="test", text="test")
+
+    slack_response_mock = {
+        'message': {
+            'teste': 'teste'
+        }
+    }
+
+    mock_client.return_value = slack_response_mock
+
+    res = asyncio.run(mock_service.post_message(payload))
+
+    assert res == slack_response_mock['message']
+
+
+def test_post_message_exception_handling(mock_service):
+    with patch('services.slack_service.WebClient.chat_postMessage', side_effect=SlackApiError(
+            "test error", {'error': 'test error'})):
+        with pytest.raises(SlackApiError):
+            payload = PostMessagePayload(channel="test", text="test")
+            asyncio.run(mock_service.post_message(payload))
